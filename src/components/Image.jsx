2a5aa555de114b6eb076f6db32c8baf72c4a9292
Praneel Maitra <praneelmaitra2006@gmail.com>
2025-07-01 22:18:03 +0530

    add api

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,17 +5,29 @@ function Image() {
     const [img,setImg] = useState('');
     const[loading, setLoading] = useState(false);
 
+    const fetchDog = async () => {
+        setLoading(true)
+        const res = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await res.json()
+        setImg(data.message)
+        setLoading(false)
+
+    }
 
   return (
-    <>
+    <div className="image">
+
+    <button className='rejectbtn' onClick={fetchDog}>❌</button>
+    
     {loading ? (<p>Loading...</p>): (
         <>
-        <button className='rejectbtn'>❌</button>
         <img src={img} alt='Random dog pic'/>
-        <button className='acceptbtn'>💓</button>
         </>
     )}
-    </>
+
+    <button className='acceptbtn' onClick={fetchDog}>💓</button>
+    </div>
+    
   )
 }
 
